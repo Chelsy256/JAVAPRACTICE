6f3c61e20ef375fd9dbb881b23a6fa53bbb47a29
Sylvia Chelangat Mwanga <sylviachelsy@gmail.com>
2025-08-20 12:02:45 +0300

    firstjavascript

---
+++ main.js
@@ -0,0 +1,75 @@
+console.log("this is my first javascript");
+// Variables
+// declaring variables using let,const or var
+
+//var
+var name = "John";
+name ="Doe" 
+console.log(name);
+
+//let
+let age= 30
+age = 30;
+console.log(age);
+
+//const
+const pi = 3.14;
+console.log(pi);
+const myName = "Sylvia"
+
+//data type
+//string(you can use single or double quotes)
+let greeting = "how are you"
+console.log(greeting);
+
+//number (integer or float)
+//integer
+let year = 2024;
+console.log(year);
+//float
+let price = 19.9;
+console.log(price);
+
+//boolean (true or false)
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+//undefined(variable declared but not assigned)
+let notAssigned;
+console.log(emptyValue);
+
+//null (explicitly no value)
+let emptyValue = null;
+console.log(emptyValue);
+
+//objects
+let person = {
+    firstName: "Sylvia",
+    lastName: "Mwanga",
+    age:30,
+    isStudent: true  
+
+}
+console.log(person);
+
+//Arrays
+let fruits = [Apple, Banana, Orange];
+console.log(fruits);
+
+let number = [1, 2, 3, 4, 5];
+console.log(number)
+
+let mixedArray = [1, "Hello", true, null];
+console.log(mixedArray);
+
+//concatenation
+let surName = "chelangat";
+let givenName = "Chela";
+let otherName = "Valu";
+const fullName = surName + " " + givenName + " " + otherName
+console.log(surName);
+console.log(givenName);
+console.log(otherName);
+
+
+
