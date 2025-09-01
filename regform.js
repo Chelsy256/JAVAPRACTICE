de9a659247683e7924c701459b593ed909dcf8cb
Sylvia Chelangat Mwanga <sylviachelsy@gmail.com>
2025-09-01 11:35:39 +0300

    regform

--- regform.js
+++ regform.js
@@ -1,1 +1,19 @@
+ // Get a form by its ID
+    //const myForm = document.getElementById('myFormId');
 
+    // Get an input field by its ID
+  //  const usernameInput = document.getElementById('username');
+
+    // Get input fields by their name attribute (returns a NodeList)
+   // const radioButtons = document.getElementsByName('gender');
+
+   const FullNameInput = document.getElementById('name')
+   console.log(FullNameInput);
+
+   const Username = document.getElementById('username')
+   console.log(Username);
+
+   const Email = document.getElementById('email')
+   console.log(Email);
+
+   const
